810d2cdad5d9658be997c9b8a5a182bd53824511
ishika <agarwal.ishika04@gmail.com>
2018-02-13 18:04:05 +0530

    some changes

--- html_scraping.py
+++ html_scraping.py
@@ -14,48 +14,68 @@ from collections import namedtuple
 fields = ['requestMethod','url','host','referer','origin','csrfToken','form','sensitiveParameters','statusCode','contentType',
          'outgoingUrl','server','x_frame_options','x_xss','content_length','comments']
 
+# for loop here..............
 #url = input("Enter a website to extract the URL's from: ")
-host = "www.webscantest.com"
-url = "www.webscantest.com/jsmenu/auto_osrun.php"
-r = ''
-Request = namedtuple('Request', fields)
-Main = []
-req1 = requests.get("http://" +url, allow_redirects = False)
-req = requests.Request('GET',"http://" +url, headers={'host':host})
-s = req.prepare()
-sessions = requests.Session()
-retry = Retry(connect=3, backoff_factor=0.5)
-adapter = HTTPAdapter(max_retries=retry)
-sessions.mount('http://', adapter)
-sessions.mount('https://', adapter)
+x = True
+while(x):
+    host = "www.webscantest.com"
+    URL = "www.webscantest.com/jsmenu/auto_osrun.php"
+    #requestMethod,url,host,referer,origin,csrfToken,form,sensitiveParameters,statusCode,contentType,
+     #    server,x_frame_options,x_xss,content_length,comments
+    outgoingUrl = []
+    r = ''
+    Request = namedtuple('Request', fields)
+    Main = []
+    req1 = requests.get("http://" +URL, allow_redirects = False)
+    req = requests.Request('GET',"http://" +URL, headers={'host':host})
+    s = req.prepare()
+    sessions = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    sessions.mount('http://', adapter)
+    sessions.mount('https://', adapter)
 
-try:
-    r = sessions.get("http://" +url)
-    print(s.path_url)
-    data = r.text
-    print("REQUEST METHOD: "+r.request.method)
-    print(s.headers['host'])
-    print("STATUS CODE:" + str(r.status_code))
-    soup = BeautifulSoup(data)
-    print(r.history)
-    print("HREFs:")
-    for link in soup.find_all('a'):
-        if link.get('href'):
-            print(link.get('href'))
-    print("SCRIPT SOURCE:")
-    for link in soup.find_all('script'):
-        if link.get('src'):
-            print(link.get('src'))
-    print("IMAGE SOURCE:")
-    for link in soup.find_all('img'):
-        if link.get('src'):
-            print(link.get('src'))
-    print("FORM ELEEMENT:")
-    for link in soup.find_all('form'):
-        if link.get('action'):
-            print(link.get('action'))
+    try:
+        r = sessions.get("http://" +URL)
+        url = s.path_url #url
+        data = r.text
+        requestMethod = r.request.method #request method
+        host = s.headers['host'] #host name
+        statusCode = r.status_code
+        server = r.headers['Server']
+        #print(server)
+        soup = BeautifulSoup(data)
+        form = False      # form tag
+        #print(r.history)
+        #print("HREFs:")
+        for link in soup.find_all('a'):
+            if link.get('href'):
+                print(link.get('href'))
+                outgoingUrl.append(link.get('href'))
+        #print("SCRIPT SOURCE:")
+        for link in soup.find_all('script'):
+            if link.get('src'):
+                print(link.get('src'))
+                outgoingUrl.append(link.get('src'))
+        #print("IMAGE SOURCE:")
+        for link in soup.find_all('img'):
+            if link.get('src'):
+                print(link.get('src'))
+                outgoingUrl.append(link.get('src'))
+        #print("FORM ELEEMENT:")
+        for link in soup.find_all('form'):
+            if link.get('action'):
+                print(link.get('action'))
+                form = True
+                outgoingUrl.append(link.get('action'))
+        for link in soup.find_all('meta'):
+            if link.get('URL'):
+                print(link.get('URL'))
+                outgoingUrl.append(link.get('URL'))
+         # LINK HREF LEFT..............
 
 
-except requests.exceptions.ConnectionError as exc:
-    print(exc)
-    sleep(5)
+    except requests.exceptions.ConnectionError as exc:
+        print(exc)
+        sleep(5)
+    x = False
